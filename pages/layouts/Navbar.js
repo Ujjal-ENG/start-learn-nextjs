e064c41175d6f31f931f11f4eb2f2317fb77a7c7
user <ujjalroy7862@gmail.com>
2023-03-19 17:09:05 +0600

    change some color on the top of the navbar when clicking

--- pages/layouts/Navbar.js
+++ pages/layouts/Navbar.js
@@ -6,16 +6,16 @@ const Navbar = () => {
     <nav>
       <ul className="flex justify-center items-center p-5 gap-10 text-3xl font-bold">
         <Link href="/home">
-          <li className="focus:text-orange-600">Home</li>
+          <li className="active:text-orange-600">Home</li>
         </Link>
         <Link href="/blog-post/blog">
-          <li className="focus:text-orange-600">Blogs</li>
+          <li className="active:text-orange-600">Blogs</li>
         </Link>
         <Link href="/about">
-          <li className="focus:text-orange-600">About</li>
+          <li className="active:text-orange-600">About</li>
         </Link>
-        <Link href="/home">
-          <li className="focus:text-orange-600">Contact Us</li>
+        <Link href="/ContactUS">
+          <li className="active:text-orange-600">Contact Us</li>
         </Link>
       </ul>
     </nav>
