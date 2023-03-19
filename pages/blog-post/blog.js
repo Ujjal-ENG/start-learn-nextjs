215a2fd73105a9a3ee746dbb482dd9c932f7ea1b
user <ujjalroy7862@gmail.com>
2023-03-19 17:58:23 +0600

    done this project right now

--- pages/blog-post/blog.js
+++ pages/blog-post/blog.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const blog = () => {
-  return <div>blog</div>;
+  return (
+    <div>
+      <h1 className="text-2xl p-4 text-center font-bold">
+        This is our First Blog from page
+      </h1>
+      <p className="break-words text-center">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
+        recusandae sunt iure sint, ut velit, eum suscipit quod voluptatum,
+        repellat quae. Sint modi nisi quasi doloremque aperiam dolor dicta
+        impedit, natus quam, voluptatibus ab itaque corrupti numquam autem
+        aliquid provident ducimus adipisci ratione blanditiis optio? Cumque
+        adipisci quasi corrupti voluptate?
+      </p>
+    </div>
+  );
 };
 
 export default blog;
